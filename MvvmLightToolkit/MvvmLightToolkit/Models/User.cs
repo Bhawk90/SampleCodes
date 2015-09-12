9972357afc6eef5eb125b431a2ddafbfb7d103e1
Gabor Gulyas <gulyas.g@outlook.com>
2015-09-12 20:57:49 +0200

    MVVM Light Toolkit - 1. rész

---
+++ MvvmLightToolkit/MvvmLightToolkit/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvvmLightToolkit.Models
+{
+    public class User : ObservableObject
+    {
+        private string _Name;
+        public string Name
+        {
+            get
+            {
+                return _Name;
+            }
+            set
+            {
+                _Name = value;
+                // RaisePropertyChanged("Name"); // A változó nevét átadva jelzi a felületnek a változást
+                RaisePropertyChanged(() => this.Name); // Ugyanaz, csak egy Lambda kifejezéssel megadva
+            }
+        }
+    }
+}
---
+++ MvvmLightToolkit/MvvmLightToolkit/ViewModel/MainViewModel.cs
@@ -0,0 +1,59 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using MvvmLightToolkit.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvvmLightToolkit.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public User User { get; set; }
+
+        public MainViewModel()
+        {
+            User = new User() { Name = "Cortana" };
+            ChangeNameCommand = new RelayCommand<string>(ChangeName, val => this.AllowChangeName == true);
+        }
+
+        #region Commands
+
+        private bool _AllowChangeName = true;
+        public bool AllowChangeName
+        {
+            get
+            {
+                return _AllowChangeName;
+            }
+            set
+            {
+                _AllowChangeName = value;
+                RaisePropertyChanged(() => this.AllowChangeName);
+                ChangeNameCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        public RelayCommand<string> ChangeNameCommand { get; private set; }
+
+        private async void ChangeName(string newName)
+        {
+            // Letiltjuk a névváltást - Ennek megváltoztatásával a gomb is letiltásra kerül
+            AllowChangeName = false;
+
+            // Várunk 1 másodpercet a név megváltoztatása előtt
+            await Task.Delay(1000);
+
+            // Megváltoztatjuk a nevet
+            User.Name = newName;
+
+            // Újra engedélyezzük a névváltást
+            AllowChangeName = true;
+        }
+
+        #endregion
+
+    }
+}
